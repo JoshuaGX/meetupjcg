4ef84464eed998c334ff09c0395a1c82917a15e5
JoshuaGX <47896703+JoshuaGX@users.noreply.github.com>
2020-01-13 15:10:50 -0700

    auth-server updates
    updated handler and serverless with the credentials

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,13 +5,13 @@ const axios = require('axios');
 module.exports.getAccessToken = async (event) => {
 
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
-    + '?client_id=YOUR_CONSUMER_KEY'
-    + '&client_secret=YOUR_CONSUMER_SECRET'
+    + '?client_id=vu25bjm17bqht16225nqij3jef'
+    + '&client_secret=a66jvcqesnsctl4iba8hn8o8oa'
     + '&grant_type=authorization_code'
-    + '&redirect_uri=YOUR_CONSUMER_REDIRECT_URI'
-    + '&code=USER_AUTHORIZATION_CODE';
+    + '&redirect_uri=https://joshuagx.github.io/meetupjcg/'
+    + '&code=d46cfe9402c58a58728271d8687e577c';
 
-  const info = await axios.post(MEETUP_OAUTH_URL);
+  const info = await axios.post(+ '&code=' + event.pathParameters.code);
 
   return {
     statusCode: 200,
